b776412c5f1d8fdce44ef437f748f3f473f9cab6
anddudkin <anddudkin000@gmail.com>
2023-09-13 01:58:51 +0300

    m

--- NeuronModels.py
+++ NeuronModels.py
@@ -1,6 +1,9 @@
 from compute import compute_ideal
 from tests import test_values
-def Neuron_Integrator(threshold):
+def Neuron_Integrator(I_in, U_tr):
+     '''I_in - Input current
+        U_tr -  max capacity of neuron (Treshold). If U_mem > U_tr neuron spikes
+          '''
      a,b=test_values()[0],test_values()[1],
      I_out = compute_ideal(a,b)
      ''' add neurons
