8175e20e69ec68353325ec8158a97f88bd37244f
anddudkin <anddudkin000@gmail.com>
2023-10-11 02:18:52 +0300

    m

--- MNIST_example.py
+++ MNIST_example.py
@@ -6,11 +6,11 @@ from datasets import MNIST_train_test, rand_in_U, encoding_to_spikes, MNIST_trai
 from NeuronModels import Neuron_IF
 import matplotlib.pyplot as plt
 
-n_neurons_out = 20
+n_neurons_out = 5
 n_neurons_in = 81
-n_train = 2
+n_train = 10
 n_test = 100
-time = 10
+time = 50
 
 conn = Connections(n_neurons_in, n_neurons_out, "all_to_all")
 conn.all_to_all_conn()
@@ -23,7 +23,7 @@ print(conn)
 
 out_neurons = Neuron_IF(n_neurons_out, n_neurons_in, 0, 140, -1, 5, traces=True)
 out_neurons.initialization()
-
+plt.ion()
 for i in range(n_train):
     print("n_train.....", i, "/", n_train)
     input_spikes = encoding_to_spikes(data_train[i][0], time)
@@ -34,6 +34,11 @@ for i in range(n_train):
         print("spikes_trace_in\n", out_neurons.spikes_trace_in)
         print("spikes_trace_out\n", out_neurons.spikes_trace_out)
         conn.update_w(out_neurons.spikes_trace_in,out_neurons.spikes_trace_out)
+        plt.matshow(conn.weights,cmap='inferno')
+        plt.draw()
+        plt.pause(1)
+        plt.clf()
+        plt.close()
     plot_U_mem(n_neurons_out, out_neurons.U_mem_trace)
 
 print(conn)
