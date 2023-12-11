bb551fc7d960db5beeabb3d002dd3d0f7e0c3c94
anddudkin <anddudkin000@gmail.com>
2023-12-11 03:08:10 +0300

    m

--- Network/f.py
+++ Network/f.py
@@ -1,6 +1,9 @@
 import badcrossbar
+import matplotlib.pyplot as plt
 
 applied_voltages = [
+    [1],
+    [1],
     [1],
     [1],
     [1],
@@ -8,9 +11,16 @@ applied_voltages = [
 
 # Device resistances in ohms.
 resistances = [
-    [345, 903, 755, 257, 646],
-    [652, 401, 508, 166, 454],
-    [442, 874, 190, 244, 635],
+    [345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755,
+     257, 646],
+    [652, 401, 508, 166, 454, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755,
+     257, 646],
+    [442, 874, 190, 244, 635, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755,
+     257, 646],
+    [442, 874, 190, 244, 635, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755,
+     257, 646],
+    [442, 874, 190, 244, 635, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755, 257, 646, 345, 903, 755,
+     257, 646],
 ]
 
 # Interconnect resistance in ohms.
@@ -21,6 +31,21 @@ solution = badcrossbar.compute(applied_voltages, resistances, r_i)
 
 # Printing the current through the word line segment to the left of device in
 # the second row and fourth column (Python uses zero-based indexing).
+import numpy as np
+
 current = solution
 print(solution[0][1])
-print(solution.voltages)
+print(solution.voltages[0])
+# plt.imshow(solution.voltages[0], cmap='gray',vmin=0, vmax=1)
+
+
+R = np.random.randint(100, 2000, (100, 100))
+V = np.ones([100, 1], dtype=int)
+
+print(R)
+print(V)
+solution1 = badcrossbar.compute(V, R, r_i)
+plt.imshow(solution1.voltages[0], cmap='gray_r', vmin=0, vmax=1)
+plt.show()
+plt.imshow(solution1.voltages[1], cmap='gray_r', vmin=0, vmax=1)
+plt.show()
