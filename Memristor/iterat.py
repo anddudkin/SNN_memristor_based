7c9ad601a30e77c19c8d87348495b84379888359
anddudkin <anddudkin000@gmail.com>
2024-05-03 16:36:31 +0300

    m

--- Memristor/iterat.py
+++ Memristor/iterat.py
@@ -8,8 +8,9 @@ import matplotlib.pyplot as plt
 
 applied_voltages = np.ones([196, 1])
 torch.set_printoptions(threshold=10_000)
-w = torch.load("C:/Users/anddu/Desktop/7сем/2_Работа/SNN-memristor-based/test/4 класаа/50_3000/tau 4/weights_tensor.pt")
-# w = torch.load("G:/Другие компьютеры/Ноутбук/7сем/2_Работа/SNN-memristor-based/test/4 класаа/50_3000/tau 4/weights_tensor.pt")
+# w = torch.load("C:/Users/anddu/Desktop/7сем/2_Работа/SNN-memristor-based/test/4 класаа/50_3000/tau 4/weights_tensor.pt")
+w = torch.load(
+    "G:/Другие компьютеры/Ноутбук/7сем/2_Работа/SNN-memristor-based/test/4 класаа/50_3000/tau 4/weights_tensor.pt")
 
 # plt.imshow(w,cmap='gray', vmin=0, vmax=1, interpolation='None')
 # plt.show()
@@ -60,19 +61,20 @@ cur = solution.currents.device
 # plt.show()
 
 
-u, ii, i_lin,g = [], [], [],[]
+u, ii, i_lin, g = [], [], [], []
 for i in range(1, 500000):
     i = i / 1000000
     u.append(i)
 
     ii.append(1 / 25000 * i * 10 ** (-1) * math.exp(5 * math.sqrt(i / 4)))
     i_lin.append(i / 25000)
-    g.append((1 / 25000 * i * 10 ** (-1) * math.exp(5 * math.sqrt(i / 4)))/i)
-vax=dict(zip(u, ii))
-print(vax.keys())
+    g.append((1 / 25000 * i * 10 ** (-1) * math.exp(5 * math.sqrt(i / 4))) / i)
+vax = dict(zip(u, ii))
+
 # plt.plot(u, ii)
 plt.plot(u, i_lin)
-plt.plot(u,ii)
+plt.plot(u, ii)
+
 
 # plt.plot(u,ii)
 
@@ -110,7 +112,6 @@ sol = None
 g_iter = None
 gr_v, gr_i, gr_g = [], [], []
 
-
 while flag:
     if cr0[0][0] > 1:
         g_g = cr0.clone().detach()
@@ -126,8 +127,8 @@ while flag:
     for i in range(len(cr0)):
         for j in range(len(cr0[0])):
             # i_lin.append(1 / 25000 * i * 10 ** (-1) * math.exp(5 * math.sqrt(i / 4)))
-            #cr0[i][j] = (crR[i][j]* 0.1 * math.exp(5 * math.sqrt(voltage[i][j] / 4)))/voltage[i][j]
-            cr0[i][j] = voltage[i][j]/vax[round(float(voltage[i][j]),6)]
+            # cr0[i][j] = (crR[i][j]* 0.1 * math.exp(5 * math.sqrt(voltage[i][j] / 4)))/voltage[i][j]
+            cr0[i][j] = voltage[i][j] / vax[round(float(voltage[i][j]), 6)]
     gr_g.append(cr0[0][0])
     det_g = torch.subtract(cr0, g_g)
 
@@ -142,32 +143,32 @@ while flag:
         # print(solution.voltages.word_line)
         sol = solution
         g_iter = cr0
-        print(solution.currents.device)
-        print(g_iter)
-plt.plot(gr_v, gr_i,"--")
-n=[]
+        # print(solution.currents.device)
+        # print(g_iter)
+plt.plot(gr_v, gr_i, "--")
+n = []
 for i in range(len(gr_v)):
     n.append(i)
 for i, txt in enumerate(n):
-    plt.annotate(txt, (gr_v[i], gr_i[i]),fontsize=12)
+    plt.annotate(txt, (gr_v[i], gr_i[i]), fontsize=12)
 plt.show()
-print(torch.add(torch.tensor(solution.voltages.word_line, dtype=torch.float),
-                torch.tensor(solution.voltages.bit_line, dtype=torch.float)))
-print(torch.mul(torch.tensor(solution.currents.device, dtype=torch.float), torch.tensor(g_iter, dtype=torch.float)))
+# print(torch.add(torch.tensor(solution.voltages.word_line, dtype=torch.float),
+#                 torch.tensor(solution.voltages.bit_line, dtype=torch.float)))
+# print(torch.mul(torch.tensor(solution.currents.device, dtype=torch.float), torch.tensor(g_iter, dtype=torch.float)))
 # print(solution.currents.device)
 i_d = torch.tensor(solution.currents.device, dtype=torch.float)
 i_bl = torch.tensor(solution.currents.bit_line, dtype=torch.float)
 i_wl = torch.tensor(solution.currents.word_line, dtype=torch.float)
 r_l = torch.ones([196, 50], dtype=torch.float)
-print(g_iter)
+# print(g_iter)
 v_d = torch.mul(i_d, g_iter)
 v_bl = torch.mul(i_bl, r_l)
 v_wl = torch.mul(i_wl, r_l)
 v_s = 0
 # for 1st bitline
-for i in range(196):
-    v_s += v_bl[i][0]
-print(v_s + v_d[0][0] + v_wl[0][0])
+# for i in range(196):
+#     v_s += v_bl[i][0]
+# print(v_s + v_d[0][0] + v_wl[0][0])
 first_col = []
 for i in range(196):
     v_s = 0
