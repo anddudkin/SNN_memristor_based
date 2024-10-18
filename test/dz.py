9b77289747b45dd02a6c8c5aa46d41bba6ba96e4
anddudkin <anddudkin000@gmail.com>
2024-10-18 14:45:21 +0300

    m

--- test/dz.py
+++ test/dz.py
@@ -8,11 +8,19 @@ import pickle
 # with open("G:/Другие компьютеры/Ноутбук/7сем/1_Магистратура/3 сем/Методы анализа микро- и наносистем/5_.txt") as f:
 #     content = f.read()
 # print(content)
-excel_data = pandas.read_excel("G:/Другие компьютеры/Ноутбук/7сем/1_Магистратура/3 сем/Методы анализа микро- и наносистем/5.xlsx",
-                                  )
+excel_data = pandas.read_excel("G:/Другие компьютеры/Ноутбук/7сем/1_Магистратура/3 сем/Методы анализа микро- и наносистем/5.xls")
 print(excel_data)
-# import matplotlib.pyplot as plt
-# import pandas
+excel_data=excel_data.drop('experimental', axis=1)
+excel_data=excel_data.drop('smoothed', axis=1)
+print(excel_data)
+for i in excel_data:
+    print(i)
+    print(excel_data[i].tolist())
+for i in excel_data:
+    if i != "channel":
+        plt.plot(excel_data["channel"].tolist(), excel_data[i].tolist(), label = i)
+plt.legend()
+plt.show()
 #
 # current, voltage, res = [], [],[]
 # for i in range(15):
