68f489a7dba1deb6b5b4132f22625175701e30db
anddudkin <anddudkin000@gmail.com>
2025-08-14 18:40:45 +0300

    m

--- Memristor/ffg.py
+++
@@ -1,6 +0,0 @@
-for p in range(11,36):
-    for x in range(0,p):
-        for y in range(p):
-            for z in range(p):
-                if (int(str(z)+str(x), p) + int(str(x)+str(y), p)) == int(str(z)+str(y)+'a', p):
-                    print(int(str(x)+str(y)+str(z), p), p)
