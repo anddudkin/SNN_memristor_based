7b076b45e6f6a32e4faf7c7493d447b5de7c3453
anddudkin <anddudkin000@gmail.com>
2025-08-15 04:08:21 +0300

    m

---
+++ Memristor/stuck_test_1.py
@@ -0,0 +1,106 @@
+import badcrossbar
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+from Network.visuals import plot_weights_square
+
+
+def g(x):
+    if x < 0.00005:
+        return 1 / 0.000005
+    else:
+        return 1 / x
+
+
+torch.set_printoptions(threshold=10_000)
+
+# Applied voltages in volts.
+
+applied_voltages = np.ones([196, 1])
+
+# w = torch.load("C:/Users/anddu/Documents/GitHub/anddudkin_mem_project/Examples/SNN_tests/weights_tensor.pt")
+# w1= torch.load("C:/Users/anddu/Documents/GitHub/anddudkin_mem_project/Examples/SNN_tests/weights_tensor.pt")
+w = torch.load("../Examples/SNN_tests/weights_tensor.pt")
+w1 = torch.load("../Examples/SNN_tests/weights_tensor.pt")
+w.apply_(g)
+w1.apply_(g)
+w = w.numpy()
+w1 = w1.numpy()
+k = 0
+for i in range(len(w1)):
+    for j in range(len(w1[0])):
+        if k < 500:
+            w1[i][j] = np.min(w1)
+        k += 1
+r_i = 1
+fig = plt.figure(figsize=(8, 8))
+axW1 = fig.add_subplot(3, 3, 1)
+axV = fig.add_subplot(3, 3, 4)
+ax1V = fig.add_subplot(3, 3, 5)
+ax2V = fig.add_subplot(3, 3, 6)
+axW2 = fig.add_subplot(3, 3, 2)
+ax = fig.add_subplot(3, 3, 7)
+ax1 = fig.add_subplot(3, 3, 8)
+ax2 = fig.add_subplot(3, 3, 9)
+axV.set_title('Voltage')
+ax.set_title('Current')
+ax1V.set_title('Voltage')
+ax1.set_title('Current')
+ax2V.set_title('delta abs(V)')
+ax2.set_title('delta I')
+# crossbars
+aximW1 = axW1.imshow(w, cmap='gray_r', interpolation='None', vmin=0,
+                     vmax=np.max(w))
+plt.colorbar(aximW1, ax=axW1, fraction=0.046, pad=0.04)
+aximW2 = axW2.imshow(w1, cmap='gray_r', interpolation='None', vmin=0,
+                     vmax=np.max(w1))
+plt.colorbar(aximW2, ax=axW2, fraction=0.046, pad=0.04)
+
+solution = badcrossbar.compute(applied_voltages, w, r_i)
+v = solution.voltages.word_line
+c = solution.currents.device
+aximV = axV.imshow(v, cmap='gray_r', interpolation='None', vmin=0,
+                   vmax=np.max(v))
+plt.colorbar(aximV, ax=axV, fraction=0.046, pad=0.04)
+
+axim = ax.imshow(solution.currents.device, cmap='gray_r', interpolation='None', vmin=0,
+                 vmax=np.max(solution.currents.device))
+plt.colorbar(axim, ax=ax, fraction=0.046, pad=0.04)
+
+solution1 = badcrossbar.compute(applied_voltages, w1, r_i)
+v1 = solution1.voltages.word_line
+c1 = solution1.currents.device
+axim1V = ax1V.imshow(v1, cmap='gray_r', interpolation='None', vmin=0,
+                     vmax=np.max(v1))
+plt.colorbar(axim1V, ax=ax1V, fraction=0.046, pad=0.04)
+
+axim1 = ax1.imshow(solution1.currents.device, cmap='gray_r', interpolation='None', vmin=0,
+                   vmax=np.max(solution1.currents.device))
+plt.colorbar(axim1, ax=ax1, fraction=0.046, pad=0.04)
+v3 = np.abs(v - v1)
+c3 = c - c1
+axim2V = ax2V.imshow(v3, interpolation='None')
+plt.colorbar(axim2V, ax=ax2V, fraction=0.046, pad=0.04)
+
+axim2 = ax2.imshow(c3, interpolation='None')
+plt.colorbar(axim2, ax=ax2, fraction=0.046, pad=0.04)
+plt.tight_layout()
+plt.subplots_adjust(bottom=0.07, left=0, right=0.85, top=0.98, wspace=0)
+
+
+diff = solution1.currents.output / solution.currents.output * 100 - 100
+print("Mean", np.mean(diff))
+print("min", np.min(diff))
+print("max", np.max(diff))
+fig1 = plt.figure(figsize=(5, 5))
+bar= fig1.add_subplot(3, 1, 1)
+bar1 = fig1.add_subplot(3, 1, 2)
+bar2 = fig1.add_subplot(3, 1, 3)
+bar.imshow(solution.currents.output, cmap='gray_r', interpolation='None')
+bar1.imshow(solution1.currents.output, cmap='gray_r', interpolation='None')
+b1 = bar2.imshow(diff, cmap='gray_r', interpolation='None')
+plt.colorbar(b1, ax=bar2, orientation='horizontal',shrink=0.5)
+fig1.tight_layout()
+plt.show()
+
